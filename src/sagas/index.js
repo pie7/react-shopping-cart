9df344f96a9fa0b65a63b8d5eb5098eb335ace37
pie7 <j3pxy6@gmail.com>
2020-05-08 18:53:30 +0800

    Add switchProduct saga

--- src/sagas/index.js
+++ src/sagas/index.js
@@ -1,6 +1,6 @@
-import { put, call, fork } from "redux-saga/effects";
+import { put, call, fork, takeEvery, take } from "redux-saga/effects";
 import axios from 'axios'
-import { FETCH_PRODUCT } from "../reducers/product";
+import { FETCH_PRODUCT, SWITCH_PRODUCT, UPDATE_SEARCH_INPUT } from "../reducers/product";
 
 const keyword = "lamborghini+car"
 
@@ -9,9 +9,24 @@ const api = (url) => axios(url)
 function* fetchProduct(action) {
     try {
         const data = yield call(api, `https://pixabay.com/api/?key=${process.env.REACT_APP_PIXABAY_API_KEY}&q=${keyword}&image_type=photo&pretty=true`)
-        yield put({ type: FETCH_PRODUCT, payload: data })
+        yield put({ type: FETCH_PRODUCT, payload: data  })
     } catch (error) {
         yield put({ type: FETCH_PRODUCT, error })
+    }
+}
+
+
+
+function* switchProduct() {
+    try {
+        while (true) {
+            const action = yield take(UPDATE_SEARCH_INPUT)
+            const { searchKeyword } = action.payload
+            const data = yield call(api, `https://pixabay.com/api/?key=${process.env.REACT_APP_PIXABAY_API_KEY}&q=${searchKeyword}&image_type=photo&pretty=true`)
+            yield put({ type: SWITCH_PRODUCT, payload: data })
+        }
+    } catch (error) {
+        yield put({ type: SWITCH_PRODUCT, error })
     }
 }
 
@@ -21,4 +36,5 @@ export function* startup() {
 
 export default function* root() {
     yield fork(startup)
+    yield fork(switchProduct)
 }
