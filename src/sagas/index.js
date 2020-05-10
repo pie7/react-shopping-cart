b288b8b98437ac2426bdba911497bb2fe3a5c193
pie7 <j3pxy6@gmail.com>
2020-05-10 20:45:00 +0800

    Update API Query Condition

--- src/sagas/index.js
+++ src/sagas/index.js
@@ -8,7 +8,7 @@ const api = (url) => axios(url)
 
 function* fetchProduct(action) {
     try {
-        const data = yield call(api, `https://pixabay.com/api/?key=${process.env.REACT_APP_PIXABAY_API_KEY}&q=${keyword}&image_type=photo&pretty=true`)
+        const data = yield call(api, `https://pixabay.com/api/?key=${process.env.REACT_APP_PIXABAY_API_KEY}&q=${keyword}&orientation=horizontal&image_type=photo&pretty=true`)
         yield put({ type: FETCH_PRODUCT, payload: data  })
     } catch (error) {
         yield put({ type: FETCH_PRODUCT, error })
@@ -22,7 +22,7 @@ function* switchProduct() {
         while (true) {
             const action = yield take(UPDATE_SEARCH_INPUT)
             const { searchKeyword } = action.payload
-            const data = yield call(api, `https://pixabay.com/api/?key=${process.env.REACT_APP_PIXABAY_API_KEY}&q=${searchKeyword}&image_type=photo&pretty=true`)
+            const data = yield call(api, `https://pixabay.com/api/?key=${process.env.REACT_APP_PIXABAY_API_KEY}&q=${searchKeyword}&orientation=horizontal&image_type=photo&pretty=true`)
             yield put({ type: SWITCH_PRODUCT, payload: data })
         }
     } catch (error) {
