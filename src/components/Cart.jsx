74638137f485a6686812144485263c826dc17588
pie7 <j3pxy6@gmail.com>
2020-05-27 09:52:06 +0800

    Add CartItem component

--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -2,74 +2,39 @@ import React from 'react';
 import PropTypes from "prop-types";
 import styled from 'styled-components';
 import { connect } from "react-redux";
-import { deleteCartItem } from "../reducers/cart";
-import Price from "./Price";
-import RadioGroup from "./RadioGroup";
-import InputStepper from "./InputStepper";
+import CartItem from "./CartItem";
 import cart_192x192 from "../assets/shopping_cart_white_192x192.png"
-import Button from './Button';
-import { ReactComponent as Close24px } from "../assets/close-24px.svg";
+import Form from "./Form";
 
 const CartContainer = styled.div`
+    display: flex;
+    flex-direction: row;
     width: 60%;
     min-width: 960px;
     padding: 32px;
-    box-sizing: border-box;
 
     @media (max-width: 768px) {
         width: 100%;
         min-width: 325px;
         padding: 16px 8px;
+        flex-direction: column;
     }
 
-    .product {
-        &__wrap {
-            display: flex;
-            flex-direction: row;
-            border: 1px solid #9e9e9e;
-            margin-bottom: 16px;
-            background: #fff;
-            padding: 16px;
-            justify-content: space-between;
-            align-items: center;
-            min-height: 150px;
-        }
-
-        &__info {
-            flex-grow: 1;
-            display: flex;
-            flex-direction: row;
-            justify-content: space-around;
-
-            @media (max-width: 768px) {
-                flex-direction: column;
-                margin-left: 8px;
-            }
-        }
-
-        &__image {
-            width: 25%;
-
-            @media (max-width: 768px) {
-                width: 50%;
-            }
-        }
-
-        &__desc,
-        &__category,
-        &__qty,
-        &__price {
-            margin-bottom: 8px;
-        }
-
-        &__id {
-            color: #9e9e9e
-        }
-    }
 
     .button--delete {
         font-size: 24px;
         color: #212121;
+    }
+
+    .cart {
+        &__items {
+            flex: 2;
+        }
+
+        &__form {
+            flex: 1;
+            max-width: 375px;
+        }
     }
 `
 
@@ -92,49 +57,20 @@ const CartEmpty = styled.div`
     }
 `
 
-const Cart = ({ cartItems = [], deleteCartItem = null }) => {
+const Cart = ({ cartItems = [] }) => {
     return (
         <CartContainer>
             {cartItems.length > 0 ?
-                cartItems.map(product => {
-                    return (
-                        <div key={product.id} className="product__wrap">
-                            <div className="product__image">
-                                <img src={product.previewURL} alt={product.title} />
-                            </div>
-                            <div className="product__info">
-                                <div className="product__desc">
-                                    <div className="product__title">
-                                        {product.title}
-                                    </div>
-                                    <div className="product__id">
-                                        {`# ${product.id}`}
-                                    </div>
-                                </div>
-                                <div className="product__category">
-                                    <RadioGroup
-                                        defaultValue={product.version}
-                                        id={product.id}
-                                        radios={product.radios}
-                                    />
-                                </div>
-                                <div className="product__qty">
-                                    <InputStepper id={product.id} />
-                                </div>
-                                <div className="product__price">
-                                    <Price amount={product.price} />
-                                </div>
-                                <div className="product__delete" >
-                                    <Button
-                                        className={'button--delete'}
-                                        clickEvent={() => deleteCartItem(product.id)}>
-                                        <Close24px />
-                                </Button>
-                                </div>
-                            </div>
-                        </div>
-                    )
-                })
+                <>
+                    <div className="cart__items">
+                        {cartItems.map(product =>
+                            <CartItem product={product} />
+                        )}
+                    </div>
+                    <div className="cart__form">
+                        <Form />
+                    </div>
+                </>
                 :
                 <CartEmpty>
                     <div className="empty__icon">
@@ -158,7 +94,5 @@ export default connect(
     state => ({
         cartItems: state.cart.cartItems
     }),
-    dispatch =>({
-        deleteCartItem: (id) => dispatch(deleteCartItem(id))
-    })
+    null
 )(Cart)
---
+++ src/components/CartItem/index.jsx
@@ -0,0 +1,111 @@
+import React from 'react';
+import styled from "styled-components"
+import PropTypes from 'prop-types';
+import Price from "./../Price";
+import RadioGroup from "./../RadioGroup";
+import InputStepper from "./../InputStepper";
+import Button from './../Button';
+import { connect } from "react-redux";
+import { ReactComponent as Close24px } from "../../assets/close-24px.svg";
+import { deleteCartItem } from "../../reducers/cart";
+
+const CartItemContainer = styled.div`
+    .product {
+        &__wrap {
+            display: flex;
+            flex-direction: row;
+            border: 1px solid #9e9e9e;
+            margin-bottom: 16px;
+            background: #fff;
+            padding: 16px;
+            justify-content: space-between;
+            align-items: center;
+            min-height: 150px;
+        }
+
+        &__info {
+            flex-grow: 1;
+            display: flex;
+            flex-direction: row;
+            justify-content: space-around;
+
+            @media (max-width: 768px) {
+                flex-direction: column;
+                margin-left: 8px;
+            }
+        }
+
+        &__image {
+            width: 25%;
+
+            @media (max-width: 768px) {
+                width: 50%;
+            }
+        }
+
+        &__desc,
+        &__category,
+        &__qty,
+        &__price {
+            margin-bottom: 8px;
+        }
+
+        &__id {
+            color: #9e9e9e
+        }
+    }
+`
+
+export const CartItem = ({ product, deleteCartItem = null }) => {
+    return (
+        <CartItemContainer>
+            <div key={product.id} className="product__wrap">
+                <div className="product__image">
+                    <img src={product.previewURL} alt={product.title} />
+                </div>
+                <div className="product__info">
+                    <div className="product__desc">
+                        <div className="product__title">
+                            {product.title}
+                        </div>
+                        <div className="product__id">
+                            {`# ${product.id}`}
+                        </div>
+                    </div>
+                    <div className="product__category">
+                        <RadioGroup
+                            defaultValue={product.version}
+                            id={product.id}
+                            radios={product.radios}
+                        />
+                    </div>
+                    <div className="product__qty">
+                        <InputStepper id={product.id} />
+                    </div>
+                    <div className="product__price">
+                        <Price amount={product.price} />
+                    </div>
+                    <div className="product__delete" >
+                        <Button
+                            className={'button--delete'}
+                            clickEvent={() => deleteCartItem(product.id)}>
+                            <Close24px />
+                        </Button>
+                    </div>
+                </div>
+            </div>
+        </CartItemContainer>
+    )
+}
+
+CartItem.propTypes = {
+    product: PropTypes.object.isRequired,
+    deleteCartItem: PropTypes.func.isRequired
+}
+
+export default connect(
+    null,
+    dispatch => ({
+        deleteCartItem: (id) => dispatch(deleteCartItem(id))
+    })
+)(CartItem)
